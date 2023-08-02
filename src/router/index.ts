b3d4344a81307dae99826a7339ad506fe0d679f6
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2023-08-02 11:07:34 -0500

    fixed routes v2

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,43 +1,45 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DefaulLayout from "../layouts/DefaultLayout.vue";
 
+const routes = [
+  {
+    path: "/",
+    name: "",
+    component: DefaulLayout,
+    children: [
+      {
+        path: "/",
+        name: "Home",
+        component: () => import("../views/Home.vue"),
+      },
+      {
+        path: "/product",
+        name: "Product",
+        component: () => import("../views/product/index.vue"),
+      },
+      {
+        path: "/product/:id",
+        name: "ProductDetail",
+        component: () => import("../views/product/detail.vue"),
+      },
+      {
+        path: "/create",
+        name: "Create",
+        component: () => import("../views/product/create.vue"),
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    name: "Auth",
+    component: () => import("../views/SignIn.vue"),
+  },
+];
+
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BAS E_URL),
   history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "",
-      component: DefaulLayout,
-      children: [
-        {
-          path: "/",
-          name: "Home",
-          component: () => import("../views/Home.vue"),
-        },
-        {
-          path: "/product",
-          name: "Product",
-          component: () => import("../views/product/index.vue"),
-        },
-        {
-          path: "/product/:id",
-          name: "ProductDetail",
-          component: () => import("../views/product/detail.vue"),
-        },
-        {
-          path: "/create",
-          name: "Create",
-          component: () => import("../views/product/create.vue"),
-        },
-      ],
-    },
-    {
-      path: "/auth",
-      name: "Auth",
-      component: () => import("../views/SignIn.vue"),
-    },
-  ],
+  routes: routes,
 });
 
 export default router;
