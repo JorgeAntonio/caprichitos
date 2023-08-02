7b9b859af87ec18cac126795d51a3ed320fe79d7
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2023-08-02 11:03:44 -0500

    fixed routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import DefaulLayout from "../layouts/DefaultLayout.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
-      name: " ",
+      name: "",
       component: DefaulLayout,
       children: [
         {
@@ -26,51 +27,16 @@ const router = createRouter({
         },
         {
           path: "/create",
-          name: "CreateProduct",
+          name: "Create",
           component: () => import("../views/product/create.vue"),
         },
       ],
     },
-    // {
-    //   path: "/dashboard",
-    //   name: "Dashboard",
-    //   component: () => import("../views/dashboard/dashboard.vue"),
-    // },
     {
       path: "/auth",
       name: "Auth",
       component: () => import("../views/SignIn.vue"),
     },
-    // {
-    //   path: "/product",
-    //   name: "Product",
-    //   component: () => import("../views/product/index.vue"),
-    // },
-    // {
-    //   path: "/product/:id",
-    //   name: "ProductDetail",
-    //   component: () => import("../views/product/detail.vue"),
-    // },
-    // {
-    //   path: "/create-product",
-    //   name: "Create_product",
-    //   component: () => import("../views/product/create.vue"),
-    // },
-    // {
-    //   path: "/registrar-ingreso",
-    //   name: "RegistrarIngreso",
-    //   component: () => import("../views/almacen/RegistrarIngreso.vue"),
-    // },
-    // {
-    //   path: "/cart",
-    //   name: "Cart",
-    //   component: () => import("../views/Cart.vue"),
-    // },
-    // {
-    //   path: "/formulario",
-    //   name: "Formulario",
-    //   component: () => import("../views/Formulario.vue"),
-    // },
   ],
 });
 
